d09ea4811b682aa374f4deab0f724f4028a9dd6b
Alexandre <alexsquin@gmail.com>
2019-05-28 16:20:49 -0300

    String1 OK

--- basic/string1.py
+++ basic/string1.py
@@ -24,8 +24,12 @@
 # So donuts(5) returns 'Number of donuts: 5'
 # and donuts(23) returns 'Number of donuts: many'
 def donuts(count):
-    # +++your code here+++
-    return
+    retorno = 'Number of donuts: '
+    if count <= 9:
+        retorno += str(count)
+    else:
+        retorno += 'many'
+    return retorno
 
 
 # B. both_ends
@@ -34,8 +38,11 @@ def donuts(count):
 # so 'spring' yields 'spng'. However, if the string length
 # is less than 2, return instead the empty string.
 def both_ends(s):
-    # +++your code here+++
-    return
+    if len(s) <= 2:
+        retorno = ''
+    else:
+        retorno = s[0] + s[1] + s[-2] + s[-1]
+    return retorno
 
 
 # C. fix_start
@@ -48,8 +55,13 @@ def both_ends(s):
 # Hint: s.replace(stra, strb) returns a version of string s
 # where all instances of stra have been replaced by strb.
 def fix_start(s):
-    # +++your code here+++
-    return
+    if len(s) > 1:
+        primeiro = s[0]
+        restante = s[1:].replace(primeiro, '*')
+        retorno = primeiro + restante
+    else:
+        retorno = ''
+    return retorno
 
 
 # D. MixUp
@@ -60,8 +72,13 @@ def fix_start(s):
 #   'dog', 'dinner' -> 'dig donner'
 # Assume a and b are length 2 or more.
 def mix_up(a, b):
-    # +++your code here+++
-    return
+    if (len(a) or len(b)) > 2:
+        a1 = b[0:2] + a[2:]
+        b1 = a[0:2] + b[2:]
+        retorno = a1 + ' ' + b1
+    else:
+        retorno = ''
+    return retorno
 
 
 # Provided simple test() function used in main() to print
